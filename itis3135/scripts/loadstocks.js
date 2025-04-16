d65630d57cb83b16ac4860c3bb8606ab46316d82
rytime13579 <61478639+rytime13579@users.noreply.github.com>
2025-04-16 15:52:37 -0400

    Fixed errors

--- itis3135/scripts/loadstocks.js
+++ itis3135/scripts/loadstocks.js
@@ -2,19 +2,17 @@ const load = document.getElementById("loadStocks");
 const containers = Array.from(document.getElementsByClassName("tip"));
 const getData = (number) => {
     const apiUrl = `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=wRXm1yrJV7pox2jYIPJYI7snlhJlXJIM`;
-
-    // Make a GET request
-    const apiData = fetch(apiUrl).then(response => {
+    const apiData = fetch(apiUrl).then((response) => {
         if (!response.ok) {
         throw new Error('Network response was not ok');
         }
         return response.json();
-    }).then(data => {
+    }).then((data) => {
         setTimeout(containers.forEach((cont) => {
             cont.innerText = `${containers.indexOf(cont) + 1}. ${data["tickers"][containers.indexOf(cont)]["ticker"]} :|: Change ${parseFloat(data["tickers"][containers.indexOf(cont)]["todaysChangePerc"]).toFixed(2)}%  :|: Change \$${parseFloat(data["tickers"][containers.indexOf(cont)]["todaysChange"]).toFixed(2)} :|: Last closing price \$${parseFloat(data["tickers"][containers.indexOf(cont)]["day"]["c"]).toFixed(2)}`;
         }),2000);
         console.log("Updated!");
-    }).catch(error => {
+    }).catch((error) => {
         console.error('Error:', error);
     });
 
