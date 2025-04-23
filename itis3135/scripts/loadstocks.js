e94b264a5d8d6ab283d4c6a80a179ebdb0d5e075
rytime13579 <61478639+rytime13579@users.noreply.github.com>
2025-04-22 20:21:10 -0400

    added api error

--- itis3135/scripts/loadstocks.js
+++ itis3135/scripts/loadstocks.js
@@ -1,15 +1,21 @@
 const load = document.getElementById("loadStocks");
 const containers = Array.from(document.getElementsByClassName("tip"));
 const getData = (number) => {
-    const apiUrl = `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=wRXm1yrJV7pox2jYIPJYI7snlhJlXJIM`;
+    const apiUrl = `https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=SCu2FspoAB0TIbpu2cvFqHO_VFtnQ4Mu`;
     const apiData = fetch(apiUrl).then((response) => {
         if (!response.ok) {
         throw new Error('Network response was not ok');
         }
         return response.json();
     }).then((data) => {
+        let x = 1;
         setTimeout(containers.forEach((cont) => {
-            cont.innerText = `${containers.indexOf(cont) + 1}. ${data["tickers"][containers.indexOf(cont)]["ticker"]} :|: Change ${parseFloat(data["tickers"][containers.indexOf(cont)]["todaysChangePerc"]).toFixed(2)}%  :|: Change \$${parseFloat(data["tickers"][containers.indexOf(cont)]["todaysChange"]).toFixed(2)} :|: Last closing price \$${parseFloat(data["tickers"][containers.indexOf(cont)]["day"]["c"]).toFixed(2)}`;
+            if (!data["tickers"][containers.indexOf(cont)]) {
+                cont.innerText = `${x}. API Error`;
+            } else {
+                cont.innerText = `${containers.indexOf(cont) + 1}. ${data["tickers"][containers.indexOf(cont)]["ticker"]} :|: Change ${parseFloat(data["tickers"][containers.indexOf(cont)]["todaysChangePerc"]).toFixed(2)}%  :|: Change \$${parseFloat(data["tickers"][containers.indexOf(cont)]["todaysChange"]).toFixed(2)} :|: Last closing price \$${parseFloat(data["tickers"][containers.indexOf(cont)]["day"]["c"]).toFixed(2)}`;
+            }
+            x++;
         }),2000);
         console.log("Updated!");
     }).catch((error) => {
@@ -19,12 +25,7 @@ const getData = (number) => {
     return apiData;
 };
 
-const updateStocksWhileLoaded = () => {
-    getData();
-    const id = window.setInterval(getData, 5000);
-    
-};
 
 window.onload = () => {
-    setTimeout(updateStocksWhileLoaded(),4000);
+    setTimeout(getData()), 4000;
 };
